20ebc36edec63e91c0f6e3c9e6533675c75c1517
ibrahim <ibrahimaydn231@gmail.com>
2024-03-31 13:27:16 +0300

    yenilendi

--- app/src/main/java/com/ibrahimaydin/firebaseeklesil/MainActivity.java
+++ app/src/main/java/com/ibrahimaydin/firebaseeklesil/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        System.out.println("sdfasf");
         textViewSicaklik = findViewById(R.id.textViewSicaklik);
         textViewNem = findViewById(R.id.textViewNem);
         textViewOksijen = findViewById(R.id.textViewOksijen);

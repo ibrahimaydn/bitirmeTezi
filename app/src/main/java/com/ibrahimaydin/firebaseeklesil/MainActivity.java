dd595db5cf68e45c0ca1541592f63dd3337f0d9c
ibrahim <ibrahimaydn231@gmail.com>
2025-08-05 23:53:43 +0300

    mobil

--- app/src/main/java/com/ibrahimaydin/firebaseeklesil/MainActivity.java
+++ app/src/main/java/com/ibrahimaydin/firebaseeklesil/MainActivity.java
@@ -41,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
+
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, API.class);
                 startActivity(intent);
